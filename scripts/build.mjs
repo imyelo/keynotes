db3aa5ad7e3eb744a8449195e3b0025a83952bfc
yelo <zhihuzeye@gmail.com>
2021-06-09 14:15:10 +0800

    feat: add index

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,4 +1,5 @@
 import fs from "fs-extra";
+import ejs from 'ejs'
 import { fileURLToPath } from "url";
 import { resolve, join } from "path";
 import { readPackageAsync } from "read-pkg";
@@ -11,19 +12,29 @@ const main = async () => {
   await fs.ensureDir(distDir);
   await fs.emptyDir(distDir);
   const pkg = await readPackageAsync({ cwd: root });
-  const redirects = []
+  const names = []
 
+  // dists
   for (let path of pkg.workspaces) {
     const projectDir = join(root, path);
     const projectPackage = await readPackageAsync({ cwd: projectDir });
     const name = projectPackage.name;
     const targetDir = join(distDir, name);
     await fs.move(join(projectDir, "dist"), targetDir);
-    redirects.push(`/${name}/*\t/${name}/index.html\t200`)
+    names.push(name)
   }
+  names.reverse(); // desc
 
+  const templates = {
+    index: await fs.readFile(join(scriptsDir, 'index.ejs'), 'utf8'),
+    redirects: await fs.readFile(join(scriptsDir, '_redirects.ejs'), 'utf8'),
+  }
+  // index
+  await fs.writeFile(join(distDir, 'index.html'), ejs.render(templates.index, { names }), 'utf8')
+  // cname
   await fs.copy(join(scriptsDir, "CNAME"), join(distDir, 'CNAME'));
-  await fs.writeFile(join(distDir, '_redirects'), redirects.join('\n'), 'utf8')
+  // redirects
+  await fs.writeFile(join(distDir, '_redirects'), ejs.render(templates.redirects, { names }), 'utf8')
 };
 
 main();
